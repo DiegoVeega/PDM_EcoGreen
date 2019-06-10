e07ea4da870a0edb0272134c68da24f29f3d9777
DiegoVeega <00148816@uca.edu.sv>
2019-06-09 19:28:09 -0600

    Repository casi completo, pruebas

--- app/src/main/java/com/genesis/ecogreen/Repository/EcoRepository.kt
+++ app/src/main/java/com/genesis/ecogreen/Repository/EcoRepository.kt
@@ -1,5 +1,6 @@
 package com.genesis.ecogreen.Repository
 
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import com.example.room_library.room.DAO.*
 import com.example.room_library.room.Entities.Project
@@ -7,8 +8,63 @@ import com.example.room_library.room.Entities.UserxProject
 import com.genesis.ecogreen.Room.Entities.Comment
 import com.genesis.ecogreen.Room.Entities.Task
 import com.genesis.ecogreen.Room.Entities.User
+import com.genesis.ecogreen.Room.Entities.UserxTask
 
 class EcoRepository(private val CommentDao : CommentDao, private val ProjectDao: ProjectDao, private val TaskDao : TaskDao, private val UserDao : UserDao, private val UserxProjectDao : UserxProjectDao, private val UserxTaskDao : UserxTaskDao) {
-    
+    //GetAll
+    val AllCommets : LiveData<List<Comment>> = CommentDao.getAllComments()
+    val AllProjects : LiveData<List<Project>> = ProjectDao.getAllProjects()
+    val AllTasks : LiveData<List<Task>> = TaskDao.getAllTasks()
+    val AllUsers : LiveData<List<User>> = UserDao.getAllUsers()
+    val AllUserxProject : LiveData<List<UserxProject>> = UserxProjectDao.getAllUserxProject()
 
+    val getAllUsersInThisProject : LiveData<List<Project>> = UserxProjectDao.getAllProjectsFromThisUser(String())
+    val getAllProjectsFromThisUser : LiveData<List<User>> = UserxProjectDao.getAllUsersInThisProject(String())
+
+    val getAllUserxTask : LiveData<List<UserxTask>> = UserxTaskDao.getAllUserxTask()
+
+    val getAllUsersInThisTask : LiveData<List<User>> = UserxTaskDao.getAllUsersInThisTask(String())
+    val getAllTasksFromThisUser : LiveData<List<Task>> = UserxTaskDao.getAllTasksFromThisUser(String())
+
+    //Comment
+    @WorkerThread
+    suspend fun insert(comment: Comment) {
+        CommentDao.insert(comment)
+    }
+    //Project
+    @WorkerThread
+    suspend fun insert(project: Project) {
+        ProjectDao.insert(project)
+    }
+
+            //Incluir funciones set de Project Equipo Genesis, </3
+
+
+
+    //Task
+    @WorkerThread
+    suspend fun insert(task: Task) {
+        TaskDao.insert(task)
+    }
+
+            //Incluir funciones set de Project Equipo Genesis, </3
+
+
+    //User
+    @WorkerThread
+    suspend fun insert(user: User) {
+        UserDao.insert(user)
+    }
+
+    //UserxProject
+    @WorkerThread
+    suspend fun insert(up: UserxProject) {
+        UserxProjectDao.insert(up)
+    }
+
+    //UserxTask
+    @WorkerThread
+    suspend fun insert(ut: UserxTask) {
+        UserxTaskDao.insert(ut)
+    }
 }
